930c5b3f224ea75bafe8444e4a18e7d7bd50f221
Juli Cheng <94julicheng@gmail.com>
2018-03-15 13:04:53 +0000

    include header and footer for index

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php require_once('../private/initialize.php'); ?>
+
+<?php $page_title = "Home"; ?>
+
+<?php include(SHARED_PATH . '/header.php'); ?>
+
+<div>
+
+</div>
+
+<?php include(SHARED_PATH . '/footer.php'); ?>
