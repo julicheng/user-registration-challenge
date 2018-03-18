c4cfcf0e79b5296ccb6ffb8b5da31a1bd28082b4
Juli Cheng <94julicheng@gmail.com>
2018-03-18 01:24:55 +0000

    add check to see if it's a post request to call sql query function

--- public/register.php
+++ public/register.php
@@ -4,7 +4,29 @@
 
 <?php include(SHARED_PATH . '/header.php'); ?>
 
+<?php 
+$errors = [];
+if(is_post_request()) {
+    $user = [];
+    $user['first_name'] = isset($_POST['first_name']) ? $_POST['first_name'] : "";
+    $user['last_name'] = isset($_POST['last_name']) ? $_POST['last_name'] : "";
+    $user['email'] = isset($_POST['email']) ? $_POST['email'] : "";
+    $user['password'] = isset($_POST['password']) ? $_POST['password'] : "";
+
+    $result = insert_user($user);
+    if($result === true) {
+        redirect_to(url_for('/login.php'));
+    } else {
+        $errors = $result;
+    }
+
+}
+
+?>
+
 <div class="content">
+
+    <?php echo display_errors($errors); ?>
 
     <form action="register.php" method="post">
 
@@ -13,7 +35,7 @@
         Last Name:<br />
         <input type="text" name="last_name" value="" /><br />
         Email:<br />
-        <input type="email" name="password" value="" /><br />
+        <input type="email" name="email" value="" /><br />
         Password:<br />
         <input type="password" name="password" value="" /><br />
         <br>
