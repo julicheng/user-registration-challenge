cf6e7473278b56425a4672cf23d14e12c8bfe329
Juli Cheng <94julicheng@gmail.com>
2018-03-18 01:27:56 +0000

    add h2 to display name of user logged in

--- public/index.php
+++ public/index.php
@@ -1,11 +1,14 @@
 <?php require_once('../private/initialize.php'); ?>
-
+<?php require_login(); ?>
 <?php $page_title = "Home"; ?>
 
 <?php include(SHARED_PATH . '/header.php'); ?>
 
-<div>
+<div class="content">
 
+    <h2>You have succesfully logged in! Good job  <?php echo $_SESSION['first_name'] . " " . $_SESSION['last_name']; ?>!</h2>
+
+    
 </div>
 
 <?php include(SHARED_PATH . '/footer.php'); ?>
