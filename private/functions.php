575a4af0f82ecbc4f4fc9562be5a4e2ebdc8f678
Juli Cheng <94julicheng@gmail.com>
2018-03-18 01:22:51 +0000

    add url_for, redirect_to, is_post_request and display_errors functions

---
+++ private/functions.php
@@ -0,0 +1,35 @@
+<?php
+
+function url_for($script_path) {
+// add the leading '/' if not present
+    if($script_path[0] != '/') {
+        $script_path = "/" . $script_path;
+    }
+    return WWW_ROOT . $script_path;
+}
+
+function redirect_to($location) {
+    header("Location: " . $location);
+    exit();
+}
+
+function is_post_request() {
+    return $_SERVER['REQUEST_METHOD'] == 'POST';
+}
+
+function display_errors($errors=[]) {
+    $output = "";
+    if(!empty($errors)) {
+        $output .= "<div class=\"errors\">";
+        $output .= "Please fix the following errors:";
+        $output .= "<ul>";
+        foreach($errors as $error) {
+            $output .= "<li>" . $error . "</li>";
+        }
+        $output .= "</ul>";
+        $output .= "</div>";
+    }
+    return $output;
+}
+
+?>
