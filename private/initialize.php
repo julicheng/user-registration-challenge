3565b3dbd890f4afa1a6eb758c4812498c46e508
Juli Cheng <94julicheng@gmail.com>
2018-03-18 01:26:49 +0000

    add function files and check if session has been started

--- private/initialize.php
+++ private/initialize.php
@@ -1,6 +1,11 @@
 <?php
 ob_start();
 
+// sessions can only start if you call session_start!
+if(!isset($_SESSION)){
+    session_start();
+}
+
 define("PRIVATE_PATH", dirname(__FILE__)); 
 define("PROJECT_PATH", dirname(PRIVATE_PATH)); 
 define("PUBLIC_PATH", PROJECT_PATH . '/public');
@@ -8,7 +13,11 @@ define("SHARED_PATH", PRIVATE_PATH . '/shared');
 
 define("WWW_ROOT", '/user-registration-challenge/public');
 
+require_once('functions.php');
 require_once('database.php');
+require_once('query_functions.php');
+require_once('auth_functions.php');
+require_once('validation_functions.php');
 
 $db = db_connect(); 
 
