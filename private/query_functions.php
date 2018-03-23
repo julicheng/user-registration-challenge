f64709ae2de6b2776278f743964e89dd296b77ea
Juli Cheng <94julicheng@gmail.com>
2018-03-23 16:35:11 +0000

    add update user query and make password and email validations

--- private/query_functions.php
+++ private/query_functions.php
@@ -52,17 +52,47 @@ function find_user_by_id($id) {
     return $user;
 }
 
-function validate_user($user) {
-    $errors = [];
+function update_user($user) {
+    global $db;
 
-    // email
-    if(is_blank($user['email'])) {
-        $errors[] = "Email cannot be blank";
-    } elseif(!has_length($user['email'], ['min' => 2, 'max' => 255])) {
-        $errors[] = "Email must be between 2 to 255 characters.";
-    } elseif(!has_unique_email($user['email'])) {
-        $errors[] = "Email already taken";
+    $password_update = !is_blank($user['password']);
+
+    $email_user = find_user_by_id($user['id']);
+
+    if ($user['email'] === $email_user['email']) {
+        $email_update = false;
+    } else {
+        $email_update = true;
     }
+
+    $errors = validate_user($user, $password_update, $email_update);
+    if(!empty($errors)) {
+        return $errors; 
+    }
+
+    $hashed_password = password_hash($user['password'], PASSWORD_BCRYPT);
+
+    $sql = "UPDATE users SET ";
+    $sql.= "first_name='" . $user['first_name'] . "', "; 
+    if($password_update) {
+        $sql.= "password='" . $hashed_password . "', ";
+    }
+    if($email_update) {
+        $sql.= "email='" . $user['email'] . "', ";
+    }
+    $sql.= "last_name='" . $user['last_name'] . "' ";
+    $sql.= "WHERE id='" . $user['id'] . "' "; 
+    $sql.= "LIMIT 1";
+
+    $result = mysqli_query($db, $sql);
+
+    if($result) {
+        return true;
+    };
+}
+
+function validate_user($user, $password_update = true, $email_update = true) {
+    $errors = [];
 
     // first name
     if(is_blank($user['first_name'])) {
@@ -78,18 +108,32 @@ function validate_user($user) {
         $errors[] = "Last name must be between 2 to 255 characters.";
     }
 
-    // password
-    if(is_blank($user['password'])) {
-        $errors[] = "Password cannot be blank";
-    } elseif(!has_length($user['password'], ['min' => 5, 'max' => 255])) {
-        $errors[] = "Password must be between 5 to 255 characters.";
+    // email
+    if(is_blank($user['email'])) {
+        $errors[] = "Email cannot be blank";
+    } elseif(!has_length($user['email'], ['min' => 2, 'max' => 255])) {
+        $errors[] = "Email must be between 2 to 255 characters.";
+    }
+    if($email_update) {
+        if (!has_unique_email($user['email'])) {
+            $errors[] = "Email already taken";
+        }
     }
 
-    // confirm password
-    if(is_blank($user['confirm_password'])) {
-        $errors[] = "Confirm password cannot be blank";
-    } elseif($user['password'] !== $user['confirm_password']) {
-        $errors[] = "Passwords do not match";
+    // password
+    if($password_update) {
+        if(is_blank($user['password'])) {
+            $errors[] = "Password cannot be blank";
+        } elseif(!has_length($user['password'], ['min' => 5, 'max' => 255])) {
+            $errors[] = "Password must be between 5 to 255 characters.";
+        }
+
+        // confirm password
+        if(is_blank($user['confirm_password'])) {
+            $errors[] = "Confirm password cannot be blank";
+        } elseif($user['password'] !== $user['confirm_password']) {
+            $errors[] = "Passwords do not match";
+        }
     }
 
     return $errors;
