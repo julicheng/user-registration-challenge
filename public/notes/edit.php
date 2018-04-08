3da972a869696eb190ee855cb734731b4263d1d6
Juli Cheng <94julicheng@gmail.com>
2018-04-08 13:36:45 +0100

    add boilerplate for notes pages

---
+++ public/notes/edit.php
@@ -0,0 +1,13 @@
+<?php require_once('../../private/initialize.php'); ?>
+<?php require_login(); ?>
+<?php $page_title = "Edit Note"; ?>
+
+<?php include(SHARED_PATH . '/header.php'); ?>
+
+<div class="content">
+
+
+    
+</div>
+
+<?php include(SHARED_PATH . '/footer.php'); ?>
