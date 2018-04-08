8512180a495bcae7740809705d0272a191682497
Juli Cheng <94julicheng@gmail.com>
2018-04-08 13:25:27 +0100

    add notes link to header

--- private/shared/header.php
+++ private/shared/header.php
@@ -22,12 +22,13 @@
 
     <navigation>
         <ul>
-             <?php if(!isset($_SESSION['first_name'])) { ?>
+             <?php if(!isset($_SESSION['id'])) { ?>
             <li><a href="<?php echo WWW_ROOT . '/login.php'; ?>">Login</a></li>
             <li><a href="<?php echo WWW_ROOT . '/register.php'; ?>">Register</a></li>
             <?php } ?>
-            <?php if(isset($_SESSION['first_name'])) { ?>
+            <?php if(isset($_SESSION['id'])) { ?>
                 <li><a href="<?php echo WWW_ROOT . '/profile.php?id=' . $_SESSION['id']; ?>">Profile</a></li>
+                <li><a href="<?php echo WWW_ROOT . '/notes.php'; ?>">Notes</a></li>
                 <li><a href="<?php echo WWW_ROOT . '/logout.php'; ?>">Logout</a></li>
             <?php } ?>
         </ul>
