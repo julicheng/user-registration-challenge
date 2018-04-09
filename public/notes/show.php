9c309613d816de39dd9f0cccae3d03a5ddf423dc
Juli Cheng <94julicheng@gmail.com>
2018-04-09 15:32:45 +0100

    use query functions in show.php to render html

--- public/notes/show.php
+++ public/notes/show.php
@@ -1,12 +1,34 @@
-<?php require_once('../../private/initialize.php'); ?>
-<?php require_login(); ?>
-<?php $page_title = "Show Note"; ?>
+<?php 
+
+require_once('../../private/initialize.php'); 
+require_login(); 
+$page_title = "Show Note"; 
+
+if(!isset($_GET['id'])) {
+    redirect_to(url_for('/notes/list.php?id=' . $_SESSION['id']));
+}
+
+$id = $_GET['id'];
+
+$note = find_note_by_id($id);
+
+?>
 
 <?php include(SHARED_PATH . '/header.php'); ?>
 
 <div class="content">
 
+    <a class="back-link" href="<?php echo url_for('/notes/list.php?id=' . $_SESSION['id']); ?>">&laquo; Back to Notes List</a>
 
+    <div>
+
+        <h1><?php echo $note['title']; ?></h1>
+
+        <div>
+                <p><?php echo $note['content']; ?></p>
+        </div>
+
+    </div>
     
 </div>
 
